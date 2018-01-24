44aefb1c916d5ea05248e0fa53982dfd973516bf
Patcharapol Nirunpornputta <patcharapol.n@ku.th>
2018-01-24 07:23:17 +0700

    Edit filterByCurrency()

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -4,37 +4,49 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A utility program of coin which can sort and filter coin by currency.
+ * 
+ * @author Patcharapol Nirunpornputta
+ *
+ */
 public class MoneyUtil {
-	
+
 	/**
 	 * Sort a coin by compareto method in purse
-	 * @param coins a list of coins.
+	 * 
+	 * @param coins
+	 *            a list of coins.
 	 */
-	public static void sortCoins(List<Coin> coins){
+	public static void sortCoins(List<Coin> coins) {
 		Collections.sort(coins);
 	}
-	
+
 	/**
+	 * Filter coin by currency and put it to a list.
 	 * 
-	 * @param coins a list of coins.
-	 * @param currency of coins
+	 * @param coins
+	 *            a list of coins.
+	 * @param currency
+	 *            of coins
 	 * @return List of coins that have same currency
 	 */
-	public static List<Coin>filterByCurrency(List<Coin>coins,String currency){
+	public static List<Coin> filterByCurrency(List<Coin> coins, String currency) {
 		List<Coin> temp = new ArrayList<Coin>();
-		for(Coin c:coins){
-			if(c.getCurrency()==currency){
+		for (Coin c : coins) {
+			if (c.getCurrency().equals(currency)) {
 				temp.add(c);
 			}
-		}return temp;
+		}
+		return temp;
 	}
-		
+
 	public static void main(String[] args) {
 		List<Coin> coins = new ArrayList<Coin>();
-		coins.add(new Coin(5.0,"Baht"));
-		coins.add(new Coin(1,"Baht"));
-		coins.add(new Coin(1,"Ringgit"));
-		coins.add(new Coin(2.5,"Dollars"));
+		coins.add(new Coin(5.0, "Baht"));
+		coins.add(new Coin(1, "Baht"));
+		coins.add(new Coin(1, "Ringgit"));
+		coins.add(new Coin(2.5, "Dollars"));
 		sortCoins(coins);
 		System.out.println(coins);
 	}
