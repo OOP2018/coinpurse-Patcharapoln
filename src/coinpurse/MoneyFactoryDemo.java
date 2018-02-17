c07371f44a57b18cc5a7a44e01c9b452e42b268e
Patcharapoln <patcharapol.n@ku.th>
2018-02-17 20:02:03 +0700

    Update MoneyFactoryTest and Demo

--- src/coinpurse/MoneyFactoryDemo.java
+++ src/coinpurse/MoneyFactoryDemo.java
@@ -1,5 +1,29 @@
 package coinpurse;
 
+/**
+ * Create ThaiMoneyFactory for testing create money.
+ * @author Patcharapol Nirunpornputta
+ *
+ */
 public class MoneyFactoryDemo {
-
+	public static void main(String[] args) {
+		MoneyFactory factory1 = MoneyFactory.getInstance();
+		MoneyFactory factory2 = MoneyFactory.getInstance();
+		System.out.println("Check that 2 factory are equal");
+		System.out.println(factory1 == factory2);
+		
+		// Create stang coin.
+		Valuable stang = factory1.createMoney(0.5);
+		System.out.println(stang.toString());
+		// Create baht coin.
+		Valuable coin = factory1.createMoney(10);
+		System.out.println(coin.toString());
+		// Create bath banknotes.
+		Valuable bankNote1 = factory1.createMoney(20);
+		Valuable bankNote2 = factory1.createMoney(100);
+		System.out.println(bankNote1.toString());
+		System.out.println(bankNote2.toString());
+		// Create invalid money it will show an error.
+		Valuable invalid = factory1.createMoney(7);
+	}
 }
