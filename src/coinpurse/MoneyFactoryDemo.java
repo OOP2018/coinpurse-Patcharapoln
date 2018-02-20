67af8bcb60cef7ceca69bce9416e9007f35e76c3
Patcharapoln <patcharapol.n@ku.th>
2018-02-20 10:25:55 +0700

    Edit MoneyFactoryDemo

--- src/coinpurse/MoneyFactoryDemo.java
+++ src/coinpurse/MoneyFactoryDemo.java
@@ -1,7 +1,7 @@
 package coinpurse;
 
 /**
- * Create ThaiMoneyFactory for testing create money.
+ * Create ThaiMoneyFactory and MalayMoneyFactory for testing create money.
  * @author Patcharapol Nirunpornputta
  *
  */
@@ -12,18 +12,30 @@ public class MoneyFactoryDemo {
 		System.out.println("Check that 2 factory are equal");
 		System.out.println(factory1 == factory2);
 		
+		System.out.println("Thai Money");
 		// Create stang coin.
 		Valuable stang = factory1.createMoney(0.5);
 		System.out.println(stang.toString());
 		// Create baht coin.
 		Valuable coin = factory1.createMoney(10);
 		System.out.println(coin.toString());
-		// Create bath banknotes.
+		// Create bath bank notes.
 		Valuable bankNote1 = factory1.createMoney(20);
 		Valuable bankNote2 = factory1.createMoney(100);
 		System.out.println(bankNote1.toString());
 		System.out.println(bankNote2.toString());
-		// Create invalid money it will show an error.
-		Valuable invalid = factory1.createMoney(7);
+		// Set factory2 to Malay.
+		factory2.setMoneyFactory(new MalayMoneyFactory());
+		factory2 = MoneyFactory.getInstance();	
+		System.out.println();
+		System.out.println("Malay Money");
+		// Create sen coin.
+		Valuable sen = factory2.createMoney(0.1);
+		System.out.println(sen.toString());
+		// Create ringgit bank notes.
+		Valuable ringgit = factory2.createMoney(10);
+		Valuable ringgit2 = factory2.createMoney(20);
+		System.out.println(ringgit.toString());
+		System.out.println(ringgit2.toString());
 	}
 }
